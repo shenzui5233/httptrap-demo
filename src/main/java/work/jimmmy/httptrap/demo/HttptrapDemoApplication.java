4c0dfff7ce23338109274902a0f9497210b075dd
shenzui5233 <zui.shen@outlook.com>
2022-10-26 02:19:45 +0800

    project init

---
+++ src/main/java/work/jimmmy/httptrap/demo/HttptrapDemoApplication.java
@@ -0,0 +1,13 @@
+package work.jimmmy.httptrap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttptrapDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttptrapDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/jimmmy/httptrap/demo/HttptrapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package work.jimmmy.httptrap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttptrapDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
